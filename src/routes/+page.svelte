344091b146ce6870f1d7ff266699a520c7ad176b
abn-oc <abdullah.iht22@gmail.com>
2025-06-02 02:32:54 -0700

    icons???

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
-    import Dividor from "$lib/dividor.svelte";
-    import Social from "$lib/social.svelte";
+	import Dividor from '$lib/dividor.svelte';
+	import Social from '$lib/social.svelte';
+	import DiGithubBadge from 'svelte-icons/di/DiGithubBadge.svelte';
+    import FaLinkedinIn from 'svelte-icons/fa/FaLinkedinIn.svelte'
 </script>
 
-
-<div class="h-screen w-screen bg-neutral-950 text-white flex flex-col">
-    <h1>Abdullah Ihtasham</h1>
-    <p>A 2nd Year CS Student from FAST NUCES Lahore. Currently focusing on web dev.</p>
-    <div class="flex flex-row gap-4">
-        <Social name="Linkedin" url="#" />
-        <Social name="Github" url="#" />
-    </div>
-    <Dividor/>
-    <h2>Projects</h2>
+<div class="flex h-screen w-screen flex-col bg-neutral-950 text-white">
+	<h1>Abdullah Ihtasham</h1>
+	<p>A 2nd Year CS Student from FAST NUCES Lahore. Currently focusing on web dev.</p>
+	<div class="flex flex-row gap-4">
+		<Social name="Linkedin" url="#" />
+		<Social name="Github" url="#" />
+	</div>
+	<Dividor />
+	<h2>Projects</h2>
 </div>
