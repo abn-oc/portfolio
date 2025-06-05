324ece57e9dc47086229c43b269a6078b72830e2
abn-oc <abdullah.iht22@gmail.com>
2025-06-04 23:35:24 -0700

    making project component

---
+++ src/lib/project.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	export let name: String;
+	export let description: String;
+	export let tags: String[];
+    export let Link: string | null;
+    export let github: string | null;
+</script>
+
+<div class="border">
+	<p class="text-bold">
+		{name}
+	</p>
+	<p class="text-neutral-500">
+		{description}
+	</p>
+	{#each tags as tag}
+		<p class="text-blue-500">tag</p>
+	{/each}
+    {#if Link}
+    <a href={Link}>linkkk</a>
+    {/if}
+    {#if github}
+    <a href={github}>githubbb</a>
+    {/if}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,36 @@
 <script lang="ts">
 	import Dividor from '$lib/dividor.svelte';
 	import Social from '$lib/social.svelte';
+	import Project from '$lib/project.svelte';
 	import FaGithub from 'svelte-icons/fa/FaGithub.svelte';
 	import FaLinkedinIn from 'svelte-icons/fa/FaLinkedinIn.svelte';
 </script>
 
 <!-- screen -->
-<div class="h-screen w-screen flex flex-col gap-8">
+<div class="flex h-screen w-screen flex-col gap-8">
 	<!-- header -->
-	<div class="flex flex-col w-[100vw] gap-4 py-8 px-24 bg-blue-800 text-white">
-			<h1 class="text-3xl">Abdullah Ihtasham</h1>
-			<p class="text-md">
-				A 2nd Year CS Student from FAST NUCES Lahore. Currently focusing on web dev. 
-			</p>
-			<div class="flex flex-row gap-4">
-				<Social name="Linkedin" url="#" icon={FaLinkedinIn} />
-				<Social name="Github" url="#" icon={FaGithub} />
-			</div>
+	<div class="flex w-[100vw] flex-col gap-4 bg-blue-800 px-24 py-8 text-white">
+		<h1 class="text-3xl">Abdullah Ihtasham</h1>
+		<p class="text-md">
+			A 2nd Year CS Student from FAST NUCES Lahore. Currently focusing on web dev.
+		</p>
+		<div class="flex flex-row gap-4">
+			<Social name="Linkedin" url="#" icon={FaLinkedinIn} />
+			<Social name="Github" url="#" icon={FaGithub} />
+		</div>
 	</div>
 	<!-- Projects -->
-	 <div class="flex flex-col px-24">
-		 <h2 class="text-2xl">Projects</h2>
-	 </div>
+	<div class="flex flex-col px-24">
+		<h2 class="text-2xl">Projects</h2>
+		<!-- projects -->
+		<div>
+			<Project
+				name="pname"
+				description="app i made"
+				tags={['skill1', 'skill2']}
+				Link=""
+				github=""
+			/>
+		</div>
+	</div>
 </div>
